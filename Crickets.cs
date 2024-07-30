8b5d64e1d1407dbfb19bb9a07c4971aff000f920
Dmitriy <icewolfex_2@mail.ru>
2024-07-30 22:31:50 +0300

    Commit

---
+++ Crickets.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Camera camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame  
+    void Update()
+    {
+        //Вывод слова "Crickets" каждый кадр.
+        Debug.Log("Crickets");
+    }
+}
